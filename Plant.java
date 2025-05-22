ea9933c3beeb029f0b0f84836e927f4abfdc2417
Mottaqi Abedin <mottaqiabedin@gmail.com>
2025-05-22 11:44:07 -0400

    created a basic skeleton for abstract Plant class

--- Plant.java
+++ Plant.java
@@ -1,3 +1,18 @@
 abstract class Plant{
+  private int cost;
+  private int startingHealth;
+  private int currHealth;
+  private Position pos;
 
+  public void UpdateHealth(int){
+
+  }
+
+  public void die(){
+
+  }
+
+  public abstract void ability(){
+    
+  }
 }
