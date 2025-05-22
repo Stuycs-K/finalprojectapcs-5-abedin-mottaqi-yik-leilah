7d0329ded9c30dd2c4dd9e23113d67d7ee8f4d20
Mottaqi Abedin <mottaqiabedin@gmail.com>
2025-05-22 11:36:39 -0400

    added instance variables

--- Game.java
+++ Game.java
@@ -1,3 +1,6 @@
 public class Game {
-  
+  private ArrayList<Plant> plants = new ArrayList<Plant>();
+  private ArrayList<Zombie> zombie = new ArrayList<Zombie>();
+  private Sun suns = new Sun();
+  private ArrayList<SoundFile> music = new ArrayList<SoundFile>();
 }
