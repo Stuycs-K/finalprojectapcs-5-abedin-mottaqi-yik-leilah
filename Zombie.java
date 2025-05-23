d649f606b3ca20668d236eae6f8bca68d721f5cd
Leilah Yik <leilahy@nycstudents.net>
2025-05-23 11:17:02 -0400

    added basic abstract zombie stuff

--- Zombie.java
+++ Zombie.java
@@ -1,3 +1,11 @@
 abstract class Zombie{
+  private int startingHealh;
+  private int currHealth;
+  private float speed;
+  private Point pos;
 
+  private void updateHealth(int dmg){};
+  private void move(){};
+  private void eat(Plant target){};
+  private void die(){};
 }
