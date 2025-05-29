cc095d4a17a67c33f75404ea7d920a4fa1e47984
Leilah Yik <leilahy@nycstudents.net>
2025-05-29 11:47:37 -0400

    worked on peashooter and working on projectile and spawnign snd stuff and peashooter sna d sunflower

--- Game.java
+++ Game.java
@@ -1,7 +1,7 @@
 public class Game {
   private ArrayList<Plant> plants = new ArrayList<Plant>();
   private ArrayList<Zombie> zombie = new ArrayList<Zombie>();
-  private Sun suns = new PlayerSun();
+  private PlayerSun suns = new PlayerSun();
   private ArrayList<SoundFile> music = new ArrayList<SoundFile>();
   private ArrayList<Waves> levels = new ArrayList<Waves>();
   private Board lawn;
--- Peashooter.java
+++ Peashooter.java
@@ -1,7 +1,8 @@
 import java.awt.Point;
 public class Peashooter extends Plant{
-  private static final int FRAMES = 60;
+  private static final int FRAMES = 81;
   private int cooldownTimer = FRAMES;
+  private 
 
   public Peashooter(Point cell){
     super(cell,100,300);
@@ -9,11 +10,24 @@ public class Peashooter extends Plant{
 
   @Override
   public void update(){
+    while(cooldownTimer > 0 && ){
+      cooldownTimer--;
+    }
+    cooldownTimer = (int) (60) * (Math.random() * .15 + 1.35);
   }
 
   @Override
   public void ability(){
     Point spawn = new Point(getPos());
     Projectile pea = new Projectile(spawn,5,20);
+  }
+
+  @Override
+  public void show(){
+
+  }
+  @Override
+  public void hide(){
+
   }
 }
