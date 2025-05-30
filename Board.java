65d88bc8ffb5c3ccf1a181ddea7595c8ca58f3e7
Mottaqi Abedin <mottaqiabedin@gmail.com>
2025-05-29 22:21:16 -0400

    made a constructor and drawGrid method

--- Board.java
+++ Board.java
@@ -1,13 +1,31 @@
+import java.util.*;
 public class Board {
     private Plant[][] plantGrid = new Plant[5][9];
     // he said to use smth else for zombies bc they wont really be on a grid
-    private int[] zombieRow;
     private ArrayList<Double> zombieRowPos;
     private ArrayList<Zombie> zombies;
-
+  
+    public Board(int rows, int cols, int screenWidth, int screenHeight){
+      plantGrid = new Plant[rows][cols];
+      zombies = new ArrayList<>();
+      zombieRowPos = new ArrayList<>();
+    }
+    
     public boolean isOccupied(int row, int col){
         if (plantGrid[row][col] == null) return false;
         return true;
+    }
+    
+    public void drawGrid() {
+      int cellWidth = 100;
+      int cellHeight = 100;
+      for (int r=0;r<plantGrid.length;r++){
+        for (int c=0;c<plantGrid[0].length;c++){
+          stroke(0);
+          fill(100,200,100);
+          rect(c*cellWidth,r*cellHeight,cellWidth,cellHeight);
+        }
+      }
     }
 
     public void placePlant(Plant plant, int[] location){
