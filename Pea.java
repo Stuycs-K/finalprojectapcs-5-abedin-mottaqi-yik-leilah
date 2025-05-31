0a4c03f0db75ff2b99bd99bdcbd7bf5d6bd4f8c7
Mottaqi Abedin <mottaqiabedin@gmail.com>
2025-05-30 21:32:58 -0400

    created Pea projectile

---
+++ Pea.java
@@ -0,0 +1,20 @@
+import java.awt.Point;
+import processing.core.PApplet;
+public class Pea extends Projectile {
+    public Pea(Point start) {
+        super(start, 4f, 20); // speed, damage
+    }
+
+    public void update(){
+        addX(getSpeed());
+        if (getX() > 900) {
+            markRemoval();
+        };
+    }
+
+    public void show(PApplet p){
+        p.fill(50, 200, 50);
+        p.ellipse(x, y, 15, 15);
+    }
+}
+
