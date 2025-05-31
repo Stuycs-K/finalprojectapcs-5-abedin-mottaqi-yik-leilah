b4979ab14a52ff4173d46034b1a95dd93d16a78e
Mottaqi Abedin <mottaqiabedin@gmail.com>
2025-05-30 22:53:31 -0400

    commented out everything for now, will add later

--- Waves.java
+++ Waves.java
@@ -1,7 +1,7 @@
 import processing.core.PApplet;
 import java.util.*;
 public class Waves {
-    private ArrayList<Zombie> zombieTypes = new ArrayList<Zombie>();
+    /*private ArrayList<Zombie> zombieTypes = new ArrayList<Zombie>();
     private ArrayList<Integer> zombieCount = new ArrayList<Integer>();
     private ArrayList<Integer> spawnTime = new ArrayList<Integer>();
     // time basically means spawn after n seconds even tho usually the condition is killing a zomb
@@ -31,5 +31,5 @@ public class Waves {
 
     public boolean waveDone(){
       return index >= zombieTypes.size();
-    }
+    }*/
 }
