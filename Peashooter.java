fadfeff06a95519562294616d714971da506d062
Mottaqi Abedin <mottaqiabedin@gmail.com>
2025-05-30 21:27:49 -0400

    oversimplified peashooter for now

--- Peashooter.java
+++ Peashooter.java
@@ -2,14 +2,33 @@ import processing.core.PApplet;
 import java.awt.Point;
 public class Peashooter extends Plant{
   // WHY IS FRAMES 81...
-  private static final int FRAMES = 81;
-  private int cooldownTimer = FRAMES;
-  private Board b;
+  // private static final int FRAMES = 81; gonna remove for now, trying to make a functioning game
+  // private int cooldownTimer = FRAMES; gonna remove for now, trying to make a functioning game
+  // private Board b; gonna remove for now, trying to make a functioning game
 
-  public Peashooter(int[] pos){
-    super(pos,300,100);
+  private int cooldown = 0;
+  private static Game gameRef; // need this for now to make it run properly
+
+  public Peashooter(int row, int col){
+    super(row,col);
   }
 
+  public void update(){
+    cooldown--;
+    if(cooldown <= 0){
+      Point peaStart = new Point(getCol() * 100 + 50, getRow() * 100 + 50);
+      Pea pea = new Pea(peaStart);
+      gameRef.addProjectile(pea);
+      cooldown = 60;
+    }
+  }
+
+  // need this for now to make it run properly
+  public static void setGame(Game g){
+    gameRef = g;
+  }
+
+  /* gonna remove for now, trying to make a functioning game
   @Override
   public void update(){
     // ADD METHOD TO CHECK IF BOARD HAS A ZOMOBIE IN IT
@@ -20,18 +39,17 @@ public class Peashooter extends Plant{
     cooldownTimer = (int) Math.round((60) * (Math.random() * .15 + 1.35));
   }
 
+  
   @Override
   public void ability(){
     Point spawn = new Point(getRow(),getCol());
     Projectile pea = new Projectile(spawn,5,20);
   }
+  */
 
   @Override
-  public void show(){
-
-  }
-  @Override
-  public void hide(){
-
+  public void show(PApplet p){
+    p.fill(0, 255, 0);
+    p.ellipse(getCol() * 100 + 50, getRow() * 100 + 50, 40, 40); 
   }
 }
