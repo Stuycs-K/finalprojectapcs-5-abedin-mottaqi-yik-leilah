41af10fd6cc8f860a52388c037abb0d717bb88ba
Mottaqi Abedin <mottaqiabedin@gmail.com>
2025-06-03 11:46:47 -0400

    added 1 wallnut sprite

--- Wallnut.java
+++ Wallnut.java
@@ -1,5 +1,7 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 public class Wallnut extends Plant{
+  private PImage full;
 
   public Wallnut(int row, int col){
     super(row,col,50,4000);
@@ -11,7 +13,10 @@ public class Wallnut extends Plant{
 
   @Override
   public void show(PApplet p){
-    p.fill(88, 57, 39);
-    p.ellipse(getCol() * 100 + 50, getRow() * 100 + 50 + 100, 40, 40);
+    full = p.loadImage("wallnut_1.png");
+    full.resize(75,75);
+    p.image(full, (float)(getCol() * 100+10), (float)(getRow() * 100 + 10 + 100));
+    // p.fill(88, 57, 39);
+    // p.ellipse(getCol() * 100 + 50, getRow() * 100 + 50 + 100, 40, 40);
   }
 }
