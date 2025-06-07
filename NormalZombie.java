c217847c3dcb03656f016ca13ec67e2235b65ef3
Mottaqi Abedin <mottaqiabedin@gmail.com>
2025-06-06 20:20:32 -0400

    added normal zombie sprites

--- NormalZombie.java
+++ NormalZombie.java
@@ -1,10 +1,16 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 import java.awt.Point;
 public class NormalZombie extends Zombie{
   private float speed = -0.27f;
   private int damageCooldown = 0;
   private static Game gameRef;
   private int damage = 100;
+  private PImage sprite;
+
+  private String fullHealth = "normalzombie_1.png";
+  private String medHealth = "normalzombie_2.png";
+  private String lowHealth = "normalzombie_3.png";
 
 
   public NormalZombie(Point start){
@@ -33,8 +39,16 @@ public class NormalZombie extends Zombie{
   }
   @Override
   public void show(PApplet p) {
-    p.fill(100, 150, 100);
-    p.rect(getX() - 20, getY() - 40, 40, 80);
+    if (getHealth() >= 120) {
+      sprite = p.loadImage(fullHealth);
+    } else if (getHealth() >= 60) {
+      sprite = p.loadImage(medHealth);
+    } else {
+      sprite = p.loadImage(lowHealth);
+    }
+    p.image(sprite, (getX() - 20), (getY() - 60));
+    //p.fill(100, 150, 100);
+    //p.rect(getX() - 20, getY() - 40, 40, 80);
   }
 
   public static void setGame(Game g){
